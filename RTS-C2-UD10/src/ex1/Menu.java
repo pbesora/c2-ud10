40ee0d551193af2286500112c5643040429662d2
pbesora <pau_besora@hotmail.com>
2022-07-18 09:50:50 +0200

    Creación menú

---
+++ RTS-C2-UD10/src/ex1/Menu.java
@@ -0,0 +1,19 @@
+package ex1;
+
+public class Menu {
+
+	private int randomValue;
+	
+	
+	private void generarValor() {
+		randomValue = (int) (Math.random()*500 + 1);
+	}
+	
+	private void comprobarValor() {
+		
+	}
+	
+	public void lanzarMenu() {
+		
+	}
+}
