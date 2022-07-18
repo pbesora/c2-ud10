816ca89774309ea692c0f4b3739dbb59c21e99e5
Bárbara Broto <barbara.broto97@gmail.com>
2022-07-18 15:46:12 +0200

    Actualización ej 5 error

--- RTS-C2-UD10/src/ex5/Password.java
+++ RTS-C2-UD10/src/ex5/Password.java
@@ -1,19 +1,28 @@
 package ex5;
 
+import java.security.SecureRandom;
+import java.util.Scanner;
+
 public class Password {
+	/**
+	 * Clase Password
+	 * Contiene información de un Password
+	 * @author Bárbara, Pau, Oriol
+	 *
+	 */
 	private int longitud;
-	private int contraseña;
+	private String contraseña;
 
 	private final int LONGITUD_D = 8;
 
 	//Constructor por defecto
 	public Password() {
 		this.longitud = LONGITUD_D;
-		this.contraseña = 0;
+		this.contraseña = "";
 	}
 
 	//Constructor pasado por parámetro
-	public Password(int longitud, int contraseña) {
+	public Password(int longitud, String contraseña) {
 		this.longitud = longitud;
 		this.contraseña = contraseña;
 	}
@@ -22,15 +31,51 @@ public class Password {
 	/* Método esFuerte
 	 * Devuelve un booleano si es fuerte o no
 	 */
-	public void esFuerte() {
+	public boolean esFuerte() {
+		int cMayus = 0;
+		int cMinus = 0;
+		int cNum = 0;
 
+		for(int i = 0; i < this.longitud; i++) {
+			char caracter = contraseña.charAt(i);
+
+			if(caracter >= 65 && caracter<=90) {
+				cMayus++;
+			} else if(caracter >= 97 && caracter <=122) {
+				cMinus++;
+			} else if (caracter >= 48 && caracter <= 57) {
+				cNum++;
+			}
+		}
+		if(cMayus >= 2 && cMinus >= 1 && cNum >= 5) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 	/* Método generarPassword
 	 * Genera la contraseña del objeto con la longitud que tenga
 	 */
 	public void generarPassword() {
+		/*Scanner sc = new Scanner(System.in);
+	        //Creamos la variable chars para tener todos los valores que pueda tener la contraseña
+	        final String chars = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789";
 
+	        SecureRandom random = new SecureRandom();
+	        StringBuilder contraseña = new StringBuilder();
+
+	        //Pedimos el rango que queremos que tenga la contraseña
+	        System.out.println("Introduce la longitud de la contraseña:");
+	        int longi = sc.nextInt();
+
+	        //Cada interacción del bucle elige aleatoriamente un carácter, y lo agrega a la instancia "StringBuilder"
+	        for(int i=0; i<longi; i++) {
+	            int randomIndex = random.nextInt(chars.length());
+	            contraseña.append(chars.charAt(randomIndex));
+	        }
+	        this.contraseña = contraseña;*/
 	}
 
 	// GETTERS Y SETTERS
@@ -42,11 +87,11 @@ public class Password {
 		this.longitud = longitud;
 	}
 
-	public int getContraseña() {
+	public String getContraseña() {
 		return contraseña;
 	}
 
-	public void setContraseña(int contraseña) {
+	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
 	}
 }
