80541b8bf673af3285800ab381a9dc7f5984676d
oandreazini <oriol_as_92@hotmail.com>
2022-07-18 11:17:34 +0200

    Creado Ex3 y Clase Numero

--- RTS-C2-UD10/src/ex3/Numero.java
+++ RTS-C2-UD10/src/ex3/Numero.java
@@ -2,8 +2,17 @@ package ex3;
 
 public class Numero {
 	
-	int numAleatorio = (int)(Math.random()*999+0);
-	
-	
-
+	public static boolean esPar(){
+		
+		int numAleatorio = (int)(Math.random()*10+0);
+		System.out.println("El número aleatorio generado es: " +numAleatorio);
+		
+	    if (numAleatorio % 2 == 0) {
+	       //Es par
+	        return true;
+	    } else {
+	    	//Es impar
+	        return false;
+	    }
+	}
 }
