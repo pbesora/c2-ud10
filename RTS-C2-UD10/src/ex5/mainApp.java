f69e3e273c05deebcf0b1f3bc1db428f36b62cbf
pbesora <pau_besora@hotmail.com>
2022-07-21 21:15:58 +0200

    Actualizado try catch ex5

--- RTS-C2-UD10/src/ex5/mainApp.java
+++ RTS-C2-UD10/src/ex5/mainApp.java
@@ -2,32 +2,53 @@ package ex5;
 
 import javax.swing.JOptionPane;
 
+import ex2.MiExcepcion;
+
 public class mainApp {
 
 	public static void main(String[] args) {
 
-		int numContraseñas = leerNumPasswords();
-		Password arrayContraseñas[] = new Password[numContraseñas];
+		int numContraseñas;
+		try {
+			numContraseñas = leerNumPasswords();
+			Password arrayContraseñas[] = new Password[numContraseñas];
 
-		int longitud = leerLongitud();
-		Boolean [] arrayEsFuerte = new Boolean[numContraseñas];
+			int longitud = leerLongitud();
+			Boolean [] arrayEsFuerte = new Boolean[numContraseñas];
+			
+			mostrarResultado(arrayContraseñas, arrayEsFuerte, longitud);
+			
+		} catch (MiExcepcion e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
 		
-		mostrarResultado(arrayContraseñas, arrayEsFuerte, longitud);
+		
+		
 	}
 	
 	//Pide el numero de contraseñas
-	private static int leerNumPasswords() {		
-		String  lectura = JOptionPane.showInputDialog ( null , "Indica cuantas contraseñas quieres crear");
-		int numContraseñas = Integer.parseInt ( lectura );
+	private static int leerNumPasswords() throws MiExcepcion {		
+		String  lectura = JOptionPane.showInputDialog ( null , "Indica cuantas contraseñas quieres crear:");
+		int numContraseñas=0;
+		
+		if(lectura.matches("[0-9]+"))
+			numContraseñas = Integer.parseInt ( lectura );
+		else
+			throw new MiExcepcion("El valor introducido no es valido.");
 		
 		return numContraseñas;
 		
 	}
 	
 	//Pide la longitud de las contraseñas
-	private static int leerLongitud() {			
-		String numCarcaters = JOptionPane.showInputDialog(null,"El número de caracteres que tendra la contraseña es: ");
-		int longitud = Integer.parseInt (numCarcaters );
+	private static int leerLongitud() throws MiExcepcion {			
+		String numCaracters = JOptionPane.showInputDialog(null,"El número de caracteres que tendra la contraseña es: ");
+		int longitud=0;
+		
+		if(numCaracters.matches("[0-9]+"))
+			longitud = Integer.parseInt (numCaracters );
+		else
+			throw new MiExcepcion("La longitud de la contraseña no es valida.");
 		
 		return longitud;
 	}
